c3a95d4b233e2f6018a807cda30ef1f1068be70c
Ryosuke Otsuya <r.otsuya@gmail.com>
2015-05-08 08:34:17 +0900

    久しぶりなので一旦コミット

---
+++ Gruntfile.js
@@ -0,0 +1,112 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    // Force use of Unix newlines
+    grunt.util.linefeed = '\n';
+
+    RegExp.quote = function (string) {
+        return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+    };
+
+    var fs = require('fs');
+    var path = require('path');
+
+    // Project configuration.
+    grunt.initConfig({
+
+        // Metadata.
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Task configuration.
+        less: {
+            'darumaya': {
+                options: {
+                    strictMath: true,
+                    sourceMap: false
+                },
+                files: [{
+                    expand: true, // 展開を有効に
+                    cwd: 'src/less/',
+                    src: ['styles.less'],
+                    dest: 'css/',
+                    ext: ".css"
+                }]
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: [
+                    'Android 2.3',
+                    'Android >= 4',
+                    'Chrome >= 20',
+                    'Firefox >= 24', // Firefox 24 is the latest ESR
+                    'Explorer >= 8',
+                    'iOS >= 6',
+                    'Opera >= 12',
+                    'Safari >= 6'
+                ]
+            },
+            dist: {
+                options: {
+                    map: false
+                },
+                src: ['css/styles.css']
+            }
+        },
+
+        csscomb: {
+            dist: {
+                expand: true,
+                cwd: 'css/',
+                src: ['styles.css'],
+                dest: 'css/'
+            }
+        },
+
+        cssmin: {
+            options: {
+                compatibility: 'ie8',
+                keepSpecialComments: '*',
+                noAdvanced: true
+            },
+            dist: {
+                files: [{
+                    expand: true, // 展開を有効に
+                    cwd: 'css/',
+                    src: ['styles.css'],
+                    dest: 'css/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        watch: {
+            'dist-css': {
+                files: 'src/less/*.less',
+                tasks: 'dist-css'
+            },
+            'dist-images': {
+                files: ['src/images/**/*.{jpg,JPG}'],
+                tasks: 'dist-images'
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
+    //require('time-grunt')(grunt);
+
+    var runSubset = function (subset) {
+        return !process.env.TWBS_TEST || process.env.TWBS_TEST === subset;
+    };
+    var isUndefOrNonZero = function (val) {
+        return val === undefined || val !== '0';
+    };
+
+    // CSS distribution task.
+    grunt.registerTask('dist-css', ['less', 'autoprefixer', 'csscomb', 'cssmin']);
+
+    // Default task.
+    grunt.registerTask('default', ['dist-css']);
+};
