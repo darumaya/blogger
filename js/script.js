baa61dc2855e9bf75a439271064e5db27a2d51e1
Ryosuke Otsuya <r.otsuya@gmail.com>
2015-08-13 23:43:11 +0900

    #11 と #28

---
+++ js/script.js
@@ -0,0 +1,68 @@
+function isBreakpoint(alias) {
+    return $('.device-' + alias).is(':visible');
+}
+
+var waitForFinalEvent = (function() {
+    var timer = 0;
+    return function(callback, duration) {
+        timer && clearTimeout(timer);
+        timer = setTimeout(callback, duration);
+    }
+})();
+
+function getDevice() {
+    return isBreakpoint('xs') ? 'xs'
+        : isBreakpoint('sm') ? 'sm'
+        : isBreakpoint('md') ? 'md'
+        : isBreakpoint('lg') ? 'lg' : '';
+}
+
+var device = getDevice();
+
+$(window).resize(function() {
+    waitForFinalEvent(function() {
+        var newDevice = getDevice();
+        if (device === newDevice) {
+            return;
+        }
+        device = newDevice;
+        if (device) {
+            $(document).trigger({
+                type: 'breakpoint',
+                device: device
+            });
+        }
+    }, 300);
+});
+
+$('.fb-like-box').attr('data-width', getFbLikeBoxWidth());
+
+function getFbLikeBoxWidth () {
+    switch (device) {
+        case 'sm':
+            return 220;
+        case 'md':
+            return 293;
+        case 'lg':
+            return 360;
+    }
+}
+
+$(document).on('breakpoint', function(event) {
+    var width = getFbLikeBoxWidth();
+    $('.fb-like-box').attr('data-width', width);
+    $('.fb-like-box > span, .fb-like-box > span > iframe').css('width', width);
+});
+
+$(document).on('click', '*[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(event.currentTarget).ekkoLightbox();
+});
+
+$(document).on('click', '.img-lightbox', function(event) {
+    var $target = $(event.currentTarget);
+    $target.ekkoLightbox({
+        remote: $target.attr('src').replace(/\-(xs|sm)/, '-md')
+    });
+});
+
